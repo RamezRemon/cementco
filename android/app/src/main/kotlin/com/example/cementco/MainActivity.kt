5003590ac1aa184dfbb5bf79296154085d733d78
RamezRemon <ramez.4230219@gmail.com>
2026-07-01 05:37:59 +0300

    First steps

---
+++ android/app/src/main/kotlin/com/example/cementco/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cementco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:cementco/welcomePage.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: ,
+    )
+  );
+}
